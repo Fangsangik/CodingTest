63039428c9a6d7a88072672cd3989c1f41952448
sangikHwang <hwangsangik@gamil.com>
2024-07-15 16:20:46 +0900

    "Review"

---
+++ src/main/java/stringReview/문자검색Re2.java
@@ -0,0 +1,26 @@
+package stringReview;
+
+import java.util.Scanner;
+
+public class 문자검색Re2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        String word = sc.nextLine();
+
+        int cnt = 0;
+        int idx = -1;
+
+        while (true) {
+            int findIdx = str.indexOf(word, idx);
+            if (findIdx < 0) {
+                break;
+            }
+
+            cnt++;
+            idx = findIdx + word.length();
+        }
+        System.out.println(cnt);
+    }
+}
---
+++ src/main/java/stringReview/문자열검색Re3.java
@@ -0,0 +1,19 @@
+package stringReview;
+
+import java.util.Scanner;
+
+public class 문자열검색Re3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String doc = sc.nextLine();
+        String word = sc.nextLine();
+
+        String replace = doc.replace(word, "");
+
+        int leftWord = doc.length() - replace.length();
+        int cnt = leftWord / word.length();
+
+        System.out.println(cnt);
+    }
+}
